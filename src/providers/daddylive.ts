ce04c9a15d817cd3fc256d7313dc1197c58c7fba
Beamlak Aschalew <birrletej12@gmail.com>
2024-02-21 10:18:00 -0800

    Added daddylive

---
+++ src/providers/daddylive.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { load } from "cheerio";
+import { ChannelEntry } from "types";
+
+export async function get247() : Promise<ChannelEntry[] | null> {
+    const channels = await axios.get("https://dlhd.sx/24-7-channels.php");
+    const $ = load(channels.data);
+    const firstGridContainer = $('.grid-container').first();
+    const gridItems = firstGridContainer.find('.grid-item').toArray();
+
+    const parsedChannels: ChannelEntry[] = [];
+    gridItems.forEach((element) => {
+        const isChannelIdValid = extractChannelId($(element).find('a').attr('href')!);
+        if (typeof isChannelIdValid !== "boolean") {
+            parsedChannels.push({id: isChannelIdValid, channel_name: $(element).find('strong').text()});
+        }
+    });
+    
+    
+    return parsedChannels;
+}
+
+function extractChannelId(text: string) : string | boolean {
+    const regex = /\/stream\/(\S+)/;
+    const match = text.match(regex);
+
+    if (match) {
+        return match[1].toString();
+    } else {
+        return false;
+    }
+}
---
+++ src/routes/daddylive.ts
@@ -0,0 +1,20 @@
+import { FastifyRequest, FastifyReply, FastifyInstance } from "fastify";
+import { get247 } from "../providers/daddylive";
+import { daddyliveReferrer, daddyliveStreamBaseUrl, daddyliveUserAgent } from "../constants/api_constants";
+
+const routes = async (fastify: FastifyInstance) => {
+    fastify.get("/", async (_, rp) => {
+        
+        rp.status(200).send({
+            intro: "Welcome to the daddylive provider",
+            routes: "/live",
+        });
+    });
+
+    fastify.get("/live", async (_, rp) => {
+        const channels = await get247();
+        rp.status(200).send({base_url: daddyliveStreamBaseUrl, referrer: daddyliveReferrer, user_agent: daddyliveUserAgent, channels: channels});
+    });
+};
+
+export default routes;
